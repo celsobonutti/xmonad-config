5a03f9a5d26c31544f671b974bfd009e5c5defc4
Celso Bonutti <i.am@cel.so>
2021-12-08 13:47:35 -0300

    feat: adds second monitor support

--- xmonad.hs
+++ xmonad.hs
@@ -3,8 +3,11 @@
 import           Graphics.X11.ExtraTypes.XF86
 import           System.IO
 import           XMonad
+import           XMonad.Actions.CycleWS
+import           XMonad.Actions.Warp
 import           XMonad.Config.Desktop
 import           XMonad.Hooks.DynamicLog
+import           XMonad.Hooks.EwmhDesktops
 import           XMonad.Hooks.ManageDocks
 import           XMonad.Layout.Hidden
 import           XMonad.Layout.NoBorders
@@ -12,29 +15,27 @@ import           XMonad.Layout.ResizableTile
 import qualified XMonad.StackSet               as W
 import           XMonad.Util.EZConfig
 import           XMonad.Util.Run                ( spawnPipe )
-import           XMonad.Hooks.EwmhDesktops
-import           XMonad.Actions.Warp
 
 main :: IO ()
 main = do
-  xmproc <- spawnPipe "xmobar /home/dokkora/.xmonad/xmobarrc"
+  xmproc0 <- spawnPipe "xmobar -x 0 /home/dokkora/.config/xmonad/xmobarrc"
+  xmproc1 <- spawnPipe "xmobar -x 1 /home/dokkora/.config/xmonad/xmobarrc-1"
   xmonad
     $ flip additionalKeys keysToAdd
     . flip removeKeys     keysToRemove
     . docks
-    $ ewmh
-        def { modMask            = myMod
+    $ ewmh def
+        { modMask            = myMod
         , terminal           = myTerminal
         , borderWidth        = 3
-        , focusedBorderColor = "purple"
+        , focusedBorderColor = "#cc20c0"
         , layoutHook         = myLayout
-        , manageHook         = manageDocks <+> manageHook desktopConfig <+> myManage
+        , manageHook = manageDocks <+> manageHook desktopConfig <+> myManage
         , logHook            = dynamicLogWithPP $ xmobarPP
-                                 { ppOutput = hPutStrLn xmproc
-                                 , ppTitle = xmobarColor "green" "" . shorten 50
-                                 }
-        , handleEventHook =
-           handleEventHook def <+> fullscreenEventHook
+          { ppOutput = \info -> hPutStrLn xmproc0 info >> hPutStrLn xmproc1 info
+          , ppTitle  = xmobarColor "green" "" . shorten 50
+          }
+        , handleEventHook    = handleEventHook def <+> fullscreenEventHook
         }
 keysToAdd =
   [ ((myMod, xK_d)                   , spawn rofiCommand)
@@ -42,8 +43,8 @@ keysToAdd =
   , ((myMod, xK_q)                   , kill)
   , ((myMod .|. shiftMask, xK_r), spawn "xmonad --recompile; xmonad --restart")
   , ((myMod .|. shiftMask, xK_Return), windows W.swapMaster)
-  , ((myMod, xK_k)                   , sequence_ [windows W.focusUp, warpToWindow 0.5 0.5])
-  , ((myMod, xK_j)                   , sequence_ [windows W.focusDown, warpToWindow 0.5 0.5])
+  , ((myMod, xK_k), sequence_ [windows W.focusUp, warpToWindow 0.5 0.5])
+  , ((myMod, xK_j), sequence_ [windows W.focusDown, warpToWindow 0.5 0.5])
   , ((myMod .|. shiftMask, xK_l)     , sendMessage Expand)
   , ((myMod .|. shiftMask, xK_h)     , sendMessage Shrink)
   , ((myMod .|. shiftMask, xK_k)     , sendMessage MirrorExpand)
@@ -52,13 +53,18 @@ keysToAdd =
   , ((myMod, xK_z)                   , popNewestHiddenWindow)
   , ((myMod, xK_l)                   , windows W.swapDown)
   , ((myMod, xK_h)                   , windows W.swapUp)
-  , ((myMod, xK_backslash)           , spawn $ myTerminal ++ "-e ranger")
-  , ((myMod, xK_Print)               , spawn "flameshot gui")
-  , ((0, xF86XK_AudioLowerVolume)    , spawn "amixer set Master 5%-")
-  , ((0, xF86XK_AudioRaiseVolume)    , spawn "amixer set Master 5%+")
-  , ((0, xF86XK_AudioMute)           , spawn "amixer set Master toggle")
-  , ((0, xF86XK_AudioPlay)           , spawn "playerctl play-pause")
-  , ((myMod, xK_backslash)           , spawn $ myTerminal <> " -e ranger")
+  , ((myMod, xK_Tab), sequence_ [nextScreen, warpToWindow 0.5 0.5])
+  , ( (myMod .|. shiftMask, xK_Tab)
+    , sequence_ [shiftNextScreen, warpToWindow 0.5 0.5]
+    )
+  , ((myMod, xK_backslash)          , spawn $ myTerminal ++ "-e ranger")
+  , ((myMod, xK_Print)              , spawn "flameshot gui")
+  , ((myMod .|. shiftMask, xK_Print), spawn "flameshot full /home/dokkora")
+  , ((0, xF86XK_AudioLowerVolume)   , spawn "amixer set Master 5%-")
+  , ((0, xF86XK_AudioRaiseVolume)   , spawn "amixer set Master 5%+")
+  , ((0, xF86XK_AudioMute)          , spawn "amixer set Master toggle")
+  , ((0, xF86XK_AudioPlay)          , spawn "playerctl play-pause")
+  , ((myMod, xK_backslash)          , spawn $ myTerminal <> " -e ranger")
   ]
 
 keysToRemove = []
@@ -73,8 +79,7 @@ myTerminal = "alacritty"
 
 myMod = mod4Mask
 
-myManage = composeAll [ title =? "Media viewer" --> doFloat
-                      ]
+myManage = composeAll [title =? "Media viewer" --> doFloat]
 
 rofiCommand =
   "rofi -modi drun -show drun -line-padding 4 -show-icons -drun-icon-theme \"Arc-X-D\" -font \"Iosevka 14\""
